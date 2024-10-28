f0850b2e8e49a4a27705bcfa78cf70a892ca88e4
sandro <noemail@lackingdomain.co>
2024-10-28 13:08:33 +0100

    added new component, delete category

---
+++ src/app/components/categories/delete/delete.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Category } from 'src/app/models/read/category.model';
+import { Router, ActivatedRoute } from '@angular/router';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { FormComponent } from '../../base/form/form.component';
+import { WebApiHttpError } from 'src/app/errors/web-api-http-error.error';
+
+@Component({
+  selector: 'delete-category',
+  templateUrl: './delete.component.html',
+  styleUrls: ['./delete.component.css']
+})
+export class DeleteCategoryComponent extends FormComponent<Category> implements OnInit, OnDestroy {
+  
+  categoryId: number | null = null;
+  category: Category | null = null;
+
+  constructor(
+    private router: Router, 
+    private categoriesService: CategoriesService
+  ) {
+    super();
+  }
+  
+  override ngOnInit(): void {
+    super.ngOnInit();
+
+    this.pathParmsChanged.subscribe(() => { 
+      const cId = this.getPathParamValueByKey("categoryId")?.toString();
+      if (!cId) {
+        this.categoryId = null;
+      } else {
+        this.categoryId = +cId;
+      }
+    });
+
+    this.categoriesService.getCategoryById(this.categoryId!).subscribe(
+      {
+        complete: () => {
+        },
+        error: (error: WebApiHttpError) => {
+          this.errors?.push(error);
+        },
+        next: (data) => {
+          this.category = data;
+          this.pageTitleService.deleteCategoryTitle = this.category.name!;
+        }
+      }
+    );
+  }
+
+  override ngOnDestroy(): void {
+    super.ngOnDestroy();
+  }
+
+  onDeleteCategoryClicked() {
+    this.categoriesService.deleteCategory(this.categoryId!).subscribe(
+      {
+        complete: () => {
+        },
+        error: (error: WebApiHttpError) => {
+          this.errors?.push(error);
+        },
+        next: () => {
+          this.router.navigate(['/categories'])
+        }
+      }
+    );
+  }
+
+}
