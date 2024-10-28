74532b95ff5c9dd7f73ded478a56c354d69d84f3
sandro <noemail@lackingdomain.co>
2024-10-28 14:07:30 +0100

    added app component and its configuration

---
+++ src/app/app.component.ts
@@ -0,0 +1,40 @@
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ToastService } from './services/toast.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit, OnDestroy {
+
+
+  toastVisible: boolean = false;
+  internalVisible: boolean = true;
+  private toastShownSubscription: Subscription | null = null;
+  private toastHiddenSubscription: Subscription | null = null;
+
+  @ViewChild('adminCredentials') admin!: ElementRef;
+
+  onAdminCredentialsPopupCloseClicked($event: Event) {
+    this.admin.nativeElement.remove();
+  }
+
+  onToastVisibilityToggled(visible: boolean): void {
+    console.log("visibility toggled: " + visible);
+    this.toastVisible = visible;
+  constructor(public toastService: ToastService) {
+    
+    this.toastVisible = false;
+  }
+
+  ngOnInit(): void {
+    this.toastVisible = false;
+  }
+
+  ngOnDestroy(): void {
+    this.toastShownSubscription?.unsubscribe();
+    this.toastHiddenSubscription?.unsubscribe();
+  }
+  }
