718afaa8889250aef2088520d1b1ff6c7f07042f
sandro <noemail@lackingdomain.co>
2024-10-28 13:18:52 +0100

    added new component, item details

---
+++ src/app/components/items/details/details.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { RoutableComponent } from '../../base/routable/routable.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { ItemsService } from 'src/app/services/items.service';
+import { Item } from 'src/app/models/read/item.model';
+import { WebApiHttpError } from 'src/app/errors/web-api-http-error.error';
+
+@Component({
+  selector: 'item-details',
+  templateUrl: './details.component.html',
+  styleUrls: ['./details.component.css']
+})
+export class ItemDetailsComponent extends RoutableComponent implements OnInit, OnDestroy {
+
+  nazivproizvoda: string | null = null;
+  item: Item | null = null;
+
+  constructor(
+    private itemsService: ItemsService,
+    public authService: AuthService
+  ) {
+    super();
+  }
+
+  override ngOnInit(): void {
+    super.ngOnInit();
+
+    this.pathParmsChanged.subscribe(() => { 
+      const iId = this.getPathParamValueByKey("itemId");
+      this.nazivproizvoda = iId;
+    });
+
+    this.itemsService.getItem(encodeURIComponent(this.nazivproizvoda!)).subscribe(
+      {
+        complete: () => {
+        },
+        error: (error: WebApiHttpError) => {
+          this.errors?.push(error);
+        },
+        next: (data) => {
+          this.item = data;
+          this.pageTitleService.itemDetailsTitle = this.item.nazivproizvoda!;
+        }
+      }
+    );
+  }
+
+  override ngOnDestroy(): void {
+    super.ngOnDestroy();
+  }
+}
