6d191cc13b2257aee3e1d8e8c28ff14de0aa2730
sandro <noemail@lackingdomain.co>
2024-10-28 11:01:18 +0100

    added new view models, which represent single entity respectively

---
+++ src/app/models/read/user.model.ts
@@ -0,0 +1,7 @@
+export class User {
+    id: number | undefined;
+    username: string | undefined;
+    roleName: string | undefined;
+    isPasswordChangeRequired: boolean | undefined;
+    roleId: number | undefined;
+}
