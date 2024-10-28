0b9faf48288d43971ccf0ab1881878d9e97f4445
sandro <noemail@lackingdomain.co>
2024-10-28 10:42:13 +0100

    added new custom global error handler implementation

---
+++ src/app/error-handling/global-error-handler.ts
@@ -0,0 +1,16 @@
+import { ErrorHandler, Injectable, Injector } from "@angular/core";
+import { ToastService } from "../services/toast.service";
+import { ToastType } from "../constants/toast-type";
+
+@Injectable()
+export class GlobalErrorHandler implements ErrorHandler {
+    constructor(private injector: Injector) {
+    }
+    handleError(error: any): void {
+        let toastService = this.injector.get(ToastService);
+
+        const message = "Unexpected error occured. Please try again.";
+        
+        toastService.show(message, ToastType.danger);
+    }
+    }
