1bbfefef82d9660b6c9e5b89eaf7950a59b19f79
sandro <noemail@lackingdomain.co>
2024-12-17 01:48:09 +0100

    updated navbar with support for locale change (custom dropdown)

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
@@ -40,10 +40,50 @@ export class NavbarComponent extends NonRoutableComponent implements OnInit, OnD
     this.authenticated = this.authService.isAuthenticated();
   }
   
-  isCollapsed: boolean = false;
+  // https://stackoverflow.com/questions/48994514/get-element-that-was-clicked
+  // TODO use angular approach
+  onExpandClicked(event: any) {
+    
+    let optionsToggle = event.closest('.link').querySelector('.dropdown-options');
+    let el = event.classList; // NEW
+    if (optionsToggle.classList.contains('active-anchor')) {
+        optionsToggle.classList.remove('active-anchor');
+        optionsToggle.classList.add('collapsed-items-dropdown-menu');
+        
+        el.remove('fa-caret-down'); // NEW
+        el.add('fa-caret-up'); // NEW
+        return;
+    } else {
+        let allOptions = document.getElementsByClassName('dropdown-options');
+        for (let i = 0, len = allOptions.length; i < len; i++) {
+          allOptions[i].classList.remove('active-anchor');
+        }
+        optionsToggle.classList.add('active-anchor');
+        el.remove('fa-caret-up'); // NEW
+        el.add('fa-caret-down'); // NEW
+    }
 
-  onHambToggle() {
-    this.isCollapsed = !this.isCollapsed;
+    const options = document.getElementsByClassName('dropdown-options');
+
+    for (let i = 0, len = options.length; i < len; i++) {
+      if (!options[i].classList.contains('.collapsed-items-dropdown-menu')) {
+          options[i].classList.add('collapsed-items-dropdown-menu');
+      }
+    }
+    let toggle = event.closest('.link').querySelector('.dropdown-options');
+    toggle.classList.remove('collapsed-items-dropdown-menu');
+  }
+
+
+  //isCollapsed: boolean = false;
+  onHambClicked(event: any) {
+    let links = document.getElementsByClassName('toggleable');
+    
+    for (let i = 0, len = links.length; i < len; i++) {
+      links[i].classList.toggle("collapsed");
+    }
+
+    // this.isCollapsed = !this.isCollapsed;
   }
 
 }
